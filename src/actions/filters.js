264d90405f14a125a77ddd006dfeeb8a95087eca
ZeinahMoualla <zeinah.moualla@gmail.com>
2018-10-05 13:28:55 +0300

    Setup Production Webpack Build

--- src/actions/filters.js
+++ src/actions/filters.js
@@ -1,49 +1,27 @@
-import moment from 'moment';
-import {
-  setStartDate,
-  setEndDate,
-  setTextFilter,
-  sortByAmount,
-  sortByDate
-} from '../../actions/filters';
-
-test('should generate set start date action object', () => {
-  const action = setStartDate(moment(0));
-  expect(action).toEqual({
-    type: 'SET_START_DATE',
-    startDate: moment(0)
-  });
+// SET_TEXT_FILTER
+export const setTextFilter = (text = '') => ({
+  type: 'SET_TEXT_FILTER',
+  text
 });
 
-test('should generate set end date aciton object', () => {
-  const action = setEndDate(moment(0));
-  expect(action).toEqual({
-    type: 'SET_END_DATE',
-    endDate: moment(0)
-  });
+// SORT_BY_DATE
+export const sortByDate = () => ({
+  type: 'SORT_BY_DATE'
 });
 
-test('should generate set text filter object with text value', () => {
-  const text = 'Something in';
-  const action = setTextFilter(text);
-  expect(action).toEqual({
-    type: 'SET_TEXT_FILTER',
-    text
-  });
+// SORT_BY_AMOUNT
+export const sortByAmount = () => ({
+  type: 'SORT_BY_AMOUNT'
 });
 
-test('should generate set text filter object with default', () => {
-  const action = setTextFilter();
-  expect(action).toEqual({
-    type: 'SET_TEXT_FILTER',
-    text: ''
-  });
+// SET_START_DATE
+export const setStartDate = (startDate) => ({
+  type: 'SET_START_DATE',
+  startDate
 });
 
-test('should generate action object for sort by date', () => {
-  expect(sortByDate()).toEqual({ type: 'SORT_BY_DATE' });
-});
-
-test('should generate action object for sort by amount', () => {
-  expect(sortByAmount()).toEqual({ type: 'SORT_BY_AMOUNT' });
+// SET_END_DATE
+export const setEndDate = (endDate) => ({
+  type: 'SET_END_DATE',
+  endDate
 });
